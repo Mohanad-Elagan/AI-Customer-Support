61abe86ffb061046b3af6872c22634397336c8f8
Mohanad-Elagan <81828865+Mohanad-Elagan@users.noreply.github.com>
2024-08-05 15:55:10 +0300

    Doneeee

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKB4TmBWfLusYu5fNifhkd3xrthxjEUi8",
-  authDomain: "fir-config-1fa50.firebaseapp.com",
-  projectId: "fir-config-1fa50",
-  storageBucket: "fir-config-1fa50.appspot.com",
-  messagingSenderId: "850975202753",
-  appId: "1:850975202753:web:85a072351d5fd1526c6340",
-  measurementId: "G-D6MDETDWYE"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
